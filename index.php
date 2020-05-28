dc6bc44ee5a4b8416da2b35ddbcb2ea5cd55ff19
Sonu Kumar <sonu.kumar@appzlogic.com>
2020-05-29 02:22:05 +0530

    now video & image both are uploaded the from local machine

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <script src="https://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
+
+  <!-- there are some issue in latest version i dont what but not working image & video -->
+  <!-- <script src="https://cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script>
+  <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script> --><!-- latest version of 4 -->
+
 </head>
 <body>
 
@@ -21,15 +26,25 @@
   <div class="row">
     <span> CK editor with image Uploader</span>
     <div class="col">
-    <textarea name="editor1"></textarea>
+    <textarea name="editor"></textarea>
                 
     </div>
   </div>
 </div>
+
 <script>
-    CKEDITOR.replace( 'editor1',{
+    // Enable local "abbr" plugin from /myplugins/abbr/ folder.
+    //this has limited feature
+    //CKEDITOR.plugins.addExternal( 'video', '/ck-editor/myplugins/video/','plugin.js' );
+
+    //this working fine for audio & video
+    CKEDITOR.plugins.addExternal( 'html5video', '/ck-editor/myplugins/ckeditor-html5-video/html5video/','plugin.js' );
+    CKEDITOR.replace( 'editor',{
         height:300,
-        filebrowserUploadUrl:'upload.php'
+        //extraPlugins:'video', //if want use then uncommnet from here
+        extraPlugins:'html5video',
+        filebrowserBrowseUrl : 'upload.php',
+        filebrowserUploadUrl : 'upload.php'
     } );
 </script> 
 </body>
